1c0c876f7195e52dc42bf0940bbd4d90a7392c19
Andy Lindsay <andydlindsay@gmail.com>
2020-01-07 19:47:37 +0000

    added sum and convertToNumbers functions

--- sum.js
+++ sum.js
@@ -1,12 +1,16 @@
 /**
-[ ] 1 need to be able to read in unlimited number of cli arguments
-[ ] 2 go through arguments
-[ ] 3 sum the arguments
-[ ] 4 store the arguments
+[x] 1 need to be able to read in unlimited number of cli arguments
+[x] 2 go through arguments
+[x] 3 sum the arguments
+[x] 4 store the arguments
 [x] 5 determine if the argument is a whole number
 [ ] 6 test for negative numbers
-[ ] 7 transform the arguments from strings to numbers
+[x] 7 transform the arguments from strings to numbers
+[ ] 8 console.log the sum
  */
+
+// need to be able to read in unlimited number of cli arguments
+const arguments = process.argv.slice(2);
 
 // an array of values
 const testWholeNumber = function (num) {
@@ -16,5 +20,29 @@ const testWholeNumber = function (num) {
   return 0;
 };
 
-console.log('7.5', testWholeNumber(7.5));
-console.log('9', testWholeNumber(9));
+// transform the arguments from strings to numbers
+const convertToNumbers = function (arr) {
+  const returnArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const value = Number(arr[i]);
+    if (value) {
+      const wholeNumber = testWholeNumber(value);
+      returnArray.push(wholeNumber);
+    };
+  }
+  return returnArray;
+};
+
+const converted = convertToNumbers(arguments);
+
+// go through arguments
+// sum the arguments
+const sum = function (arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+};
+
+console.log(sum(converted));
